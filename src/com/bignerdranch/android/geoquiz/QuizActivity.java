a2941820af875d2247c1df185236d347882041c8
Trevor B Adams <trevorbadams@gmail.com>
2014-02-25 21:33:39 -0800

    Added questions and Javadoc.

--- src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -9,14 +9,26 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/**
+ * An activity consisting of a quiz on various geography questions.  Users are informed by toasts
+ * whether there answers are correct, and buttons allow them to move forward and backwards through
+ * the questions.
+ * @author Tadams
+ */
 public class QuizActivity extends Activity {
 	
+  /*
+   *  Screen elements.
+   */
 	private Button mTrueButton;
 	private Button mFalseButton;
 	private ImageButton mNextButton;
 	private ImageButton mPrevButton;
 	private TextView mQuestionTextView;
 	
+	/*
+	 * Array containing questions to be asked by activity, in the forms of TrueFalse objects.
+	 */
 	private TrueFalse[] mQuestionBank = new TrueFalse[] {
 	    new TrueFalse(R.string.question_oceans, true),
 	    new TrueFalse(R.string.question_mideast, false),
@@ -27,6 +39,11 @@ public class QuizActivity extends Activity {
 	
 	private int mCurrentIndex = 0;
 
+	/**
+	 * Creates the activity, setting up the initial view and the behavior of the Buttons.
+	 * 
+	 * @param savedInstanceState instance information used to restore activity to its previous state. 
+	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -69,6 +86,9 @@ public class QuizActivity extends Activity {
 		updateQuestion();
 	}
 
+	/**
+	 * Inflates the options menu.
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
--- src/com/bignerdranch/android/geoquiz/TrueFalse.java
+++ src/com/bignerdranch/android/geoquiz/TrueFalse.java
@@ -6,23 +6,40 @@ public class TrueFalse {
 	
 	private boolean mTrueQuestion;
 	
+	/**
+	 * Creates a True/False question object.
+	 * @param question Resource ID of the question's string resource
+	 * @param trueQuestion the truth value of the question's answer
+	 */
 	public TrueFalse(int question, boolean trueQuestion){
 		mQuestion = question;
 		mTrueQuestion = trueQuestion;
 	}
 
+	/**
+	 * @return question's text resource ID
+	 */
 	public int getQuestion() {
 		return mQuestion;
 	}
 
+	/**
+	 * @param question set resource ID of question's text
+	 */
 	public void setQuestion(int question) {
 		mQuestion = question;
 	}
 
+	/**
+	 * @return whether the statement is true or false
+	 */
 	public boolean isTrueQuestion() {
 		return mTrueQuestion;
 	}
 
+	/**
+	 * @param trueQuestion set whether the statement is true or false
+	 */
 	public void setTrueQuestion(boolean trueQuestion) {
 		mTrueQuestion = trueQuestion;
 	}
